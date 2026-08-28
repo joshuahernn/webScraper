5cfeae9c1a341f39fa0edb9db0c559a6733dfc3c
Joshua Hernandez <JoshuaHernn@gmail.com>
2026-08-27 20:02:30 -0700

    Implemented status code flags meant for redirection / stoppage. Logic still needs to be implemented

--- webScrapper.py
+++ webScrapper.py
@@ -20,38 +20,60 @@
 
 import requests # requests import is able to take care of the TCP connection and do HTTP get, the server sends us an HTTP responses, and the body of that response contains HTML. 
 from bs4 import BeautifulSoup
+import networkx as nx
 
 url = "https://www.NBA.com" 
 headers = {
     'user-agent': 'my-app/0.0.1'
     }
 
-r = requests.get(url, headers=headers)
+r = requests.head(url, allow_redirects=True) #
+r = requests.options("https://www.NBA.com/robots.txt")
+r.headers.get('content-type') 
 
-r = requests.options("https://www.NBA.com")
+soup = BeautifulSoup(r.text, 'lxml')
 
-r.headers.get('content-type')
+# print(soup.get_text()) # TODO: Store this 
 
-print(r.headers)
-
-# Currently, this is able to return the headers of the domain that we select. The header is returned in raw HTML, so our next step would be to use BS4 to process this the HTML
-# TODO: 1. Check for status codes, check for roboot.txt, What to do when we get back JSON, check for what body of text you recieved.
-
-# 8/27/26: Read over more documentiaion
-
- 
+# for link in soup.find_all('a'): # TODO: Store this
+ #   print(link.get('href'))
 
 
+# print(r.status_code) FOR DEBUG
 
 
+client_error = ["400","401","403","404","429"] # most common is going to be 403: forbidden
+lines = soup.prettify().splitlines()
+
+client_errors = [400, 401, 403, 404, 429]
+server_errors = [500, 502, 503, 504]
+
+
+if r.status_code in client_errors :
+    print("Client Error has been found:",r.status_code)  # web crawler stops, should redirect or kill process.
+if r.status_code in server_errors :
+    print("Server Error has been found:",r.status_code)
 
 
 
 '''
-def webScrapper ():
-    return 
+When crawling, you need to collect multiple things. 
+1. First, the raw text of the page.  This means you don’t want the entire HTML file, but rather, just the important bits of text. 
+2. Second, the URL of the page you got the URL from. You will need the URL to make sure you don’t crawl the same page twice. You will also need the URL to ensure you can retrieve the original page again. 
+3. Finally, when crawling, keep an adjacency matrix so you know which pages link to others. This will be directed, as a page might point to another, but not visa versa.
+'''
 
 '''
+# 8/27/26 notes: 
+We are able to ping a domain and recieve a status code. I have set flags to come back for when we get stopped,
+but I need to wrap this around redirection / stop logic so when the crawler gets stopped it doesn't crash.
+
+The start of requirements #1 and #2 are there, but I need to figure out how to use pandas to store that into a dictionary.
+
+'''
+
+
+
 
 
 
